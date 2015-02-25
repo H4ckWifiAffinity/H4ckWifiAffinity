e817951e9149716aa4c87399e783b5c378f170db
Luis de Dios Martín <luisddm@gmail.com>
2015-02-25 10:53:47 +0100

    Modified regex expressions in order to get valid JSON

--- router/interface_dump.sh
+++ router/interface_dump.sh
@@ -9,26 +9,26 @@ server_full_url=$server_protocol$server_url':'$server_port
 
 function parse_response {
 	local time=$1
-	local signal=`echo $@ | grep -o '[^ ,]\+ signal'`
-	local noise=`echo $@ | grep -o '[^ ,]\+ noise'`
-	local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+'`
-	local da=`echo $@ | grep -o 'DA:[^ ,]\+'`
-	local sa=`echo $@ | grep -o 'SA:[^ ,]\+'`
-	local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[1-9]\+'`
-	local channel=`echo $@ | grep -o 'CH: [^ ,]\+' | grep -o '[1-9]\+'`
-	local json_data='{package_type: RESPONSE, time: '$time', signal: '$signal', noise: '$noise', BSSID: '$bssid', DA: '$da', SA: '$sa', freq: '$freq', channel: '$channel'}'
+	local signal=`echo $@ | grep -o '[^ ,]\+ signal' | grep -o '\-[0-9]\+'`
+	local noise=`echo $@ | grep -o '[^ ,]\+ noise' | grep -o '\-[0-9]\+'`
+	local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+' | cut -c 7-`
+	local da=`echo $@ | grep -o 'DA:[^ ,]\+' | cut -c 4-`
+	local sa=`echo $@ | grep -o 'SA:[^ ,]\+' | cut -c 4-`
+	local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[0-9]\+'`
+	local channel=`echo $@ | grep -o 'CH: [^ ,]\+' | grep -o '[0-9]\+'`
+	local json_data='{"type": "response", "time": "'$time'", "signal": '$signal', "noise": '$noise', "bssid": "'$bssid'", "dst": "'$da'", "src": "'$sa'", "freq": '$freq', "ch": '$channel'}'
 	send_data $json_data
 }
 
 function parse_request {
 	local time=$1
-	local signal=`echo $@ | grep -o '[^ ,]\+ signal'`
-	local noise=`echo $@ | grep -o '[^ ,]\+ noise'`
-	local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+'`
-	local da=`echo $@ | grep -o 'DA:[^ ,]\+'`
-	local sa=`echo $@ | grep -o 'SA:[^ ,]\+'`
-	local freq=`echo $@ | grep -o '[^ ,]\+ MHz'`
-	local json_data='{package_type: REQUEST, time: '$time', signal: '$signal', noise: '$noise', BSSID: '$bssid', DA: '$da', SA: '$sa', freq: '$freq', channel: '$channel'}'
+	local signal=`echo $@ | grep -o '[^ ,]\+ signal' | grep -o '\-[0-9]\+'`
+	local noise=`echo $@ | grep -o '[^ ,]\+ noise' | grep -o '\-[0-9]\+'`
+	local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+' | cut -c 7-`
+	local da=`echo $@ | grep -o 'DA:[^ ,]\+' | cut -c 4-`
+	local sa=`echo $@ | grep -o 'SA:[^ ,]\+' | cut -c 4-`
+	local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[0-9]\+'`
+	local json_data='{"type": "request", "time": "'$time'", "signal": '$signal', "noise": '$noise', "bssid": "'$bssid'", "dst": "'$da'", "src": "'$sa'", "freq": '$freq'}'
 	send_data $json_data
 }
 
