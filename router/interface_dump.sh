351fc869016d84872d2ec7ca3d48cf3b881ff42b
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-03-18 15:34:31 +0100

    Changed back to send every package the router listen to

--- router/interface_dump.sh
+++ router/interface_dump.sh
@@ -27,7 +27,8 @@ function parse_response {
 		local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[0-9]\+'`
 		local channel=`echo $@ | grep -o 'CH: [^ ,]\+' | grep -o '[0-9]\+'`
 		local json_data='{"type":"response","time":"'$time'","signal":'$signal',"noise":'$noise',"bssid":"'$bssid'","dst":"'$da'","src":"'$sa'","freq":'$freq',"ch":'$channel'}'
-		json_array=$json_array$json_data","
+		# json_array=$json_array$json_data","
+		send_data $json_data
 	fi
 }
 
@@ -46,42 +47,52 @@ function parse_request {
 		local sa=`echo $@ | grep -o 'SA:[^ ,]\+' | cut -c 4-`
 		local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[0-9]\+'`
 		local json_data='{"type":"request","time":"'$time'","signal":'$signal',"noise":'$noise',"bssid":"'$bssid'","dst":"'$da'","src":"'$sa'","freq":'$freq'}'
-		json_array=$json_array$json_data","
+		# json_array=$json_array$json_data","
+		send_data $json_data
 	fi
 }
 
 function send_data {
-	json_array=${json_array::-1}']'
-	#echo $json_array
+	#json_array=${json_array::-1}']'
+
+	# echo $json_data
+
 	curl -i \
 	-H "Content-Type:application/json" \
-	-X POST --data $json_array \
+	-X POST --data $json_data \
 	$server_full_url'/api/adddump/'
-	json_array=0
+	json_array=0-9
 }
 
 
 function start_dumping () {
-	local starttime="$(date +%s)"
+	#local starttime="$(date +%s)"
 	$dump_command | while read dump_output; do
-	   if [[ $dump_output == *"Response"* ]]
-	   	then
-	   	parse_response $dump_output
-	   elif [[ $dump_output == *"Request"* ]]; then
-	   	parse_request $dump_output
-	   fi
-	   local now="$(date +%s)"
-	   local elapsed=$((now  - starttime))
+
+		if [[ $dump_output == *"Request"* ]]
+	   		then
+	   		parse_request $dump_output
+	   	fi
+
+	   # if [[ $dump_output == *"Response"* ]]
+	   # 	then
+	   # 	parse_response $dump_output
+	   # elif [[ $dump_output == *"Request"* ]]; then
+	   # 	parse_request $dump_output
+	   # fi
+
+	   #local now="$(date +%s)"
+	   #local elapsed=$((now  - starttime))
 	   #echo $elapsed
-	   if [ $elapsed -gt $working_seconds ];
-	   	then
-	   	send_data
-	   	return
-	   fi
+	   #if [ $elapsed -gt $working_seconds ];
+	   	# then
+	   	# send_data
+	   	# return
+	   # fi
 	done	
-	echo "Waiting "$idle_seconds" seconds"
-	sleep $idle_seconds
-   	start_dumping	
+	# echo "Waiting "$idle_seconds" seconds"
+	# sleep $idle_seconds
+ #   	start_dumping	
 }
 
 start_dumping
