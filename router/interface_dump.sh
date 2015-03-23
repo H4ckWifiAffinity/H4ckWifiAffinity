331f86b70c8d84f82dc6a171a76ce8568613d10a
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-03-23 11:20:14 +0100

    Changed the script to recive interface and identifier as parameters; send only request and change timestamp format

--- router/interface_dump.sh
+++ router/interface_dump.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-interface=wlan0
+interface=$1
 dump_command='tcpdump -e -s 256 type mgt subtype probe-resp or subtype probe-req -I -i '$interface
+router=$2
+# TODO: Validate input parameters
 working_seconds=10
 idle_seconds=20
 server_protocol='http://'
@@ -11,88 +13,43 @@ server_full_url=$server_protocol$server_url':'$server_port
 
 json_array="["
 
-function parse_response {
-	local signal=`echo $@ | grep -o '[^ ,]\+ signal' | grep -o '\-[0-9]\+'`
-	if ! [ -z "$signal" ];
-	then
-		local time=$1
-		local noise=`echo $@ | grep -o '[^ ,]\+ noise' | grep -o '\-[0-9]\+'`
-		if [ -z "$noise" ];
-		then
-			local noise=-100
-		fi
-		local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+' | cut -c 7-`
-		local da=`echo $@ | grep -o 'DA:[^ ,]\+' | cut -c 4-`
-		local sa=`echo $@ | grep -o 'SA:[^ ,]\+' | cut -c 4-`
-		local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[0-9]\+'`
-		local channel=`echo $@ | grep -o 'CH: [^ ,]\+' | grep -o '[0-9]\+'`
-		local json_data='{"type":"response","time":"'$time'","signal":'$signal',"noise":'$noise',"bssid":"'$bssid'","dst":"'$da'","src":"'$sa'","freq":'$freq',"ch":'$channel'}'
-		# json_array=$json_array$json_data","
-		send_data $json_data
-	fi
-}
-
 function parse_request {
 	local signal=`echo $@ | grep -o '[^ ,]\+ signal' | grep -o '\-[0-9]\+'`
 	if ! [ -z "$signal" ];
 	then
-		local time=$1
-		local noise=`echo $@ | grep -o '[^ ,]\+ noise' | grep -o '\-[0-9]\+'`
-		if [ -z "$noise" ];
-		then
-			local noise=-100
-		fi
+		local time="$(date +%s)"
+		# local router= `/sbin/ifconfig | grep \'$interface\' | tr -s ' ' | cut -d ' ' -f5`
+		# local router=$(ifconfig wlan0 | grep -o -E "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}")
 		local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+' | cut -c 7-`
 		local da=`echo $@ | grep -o 'DA:[^ ,]\+' | cut -c 4-`
 		local sa=`echo $@ | grep -o 'SA:[^ ,]\+' | cut -c 4-`
 		local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[0-9]\+'`
-		local json_data='{"type":"request","time":"'$time'","signal":'$signal',"noise":'$noise',"bssid":"'$bssid'","dst":"'$da'","src":"'$sa'","freq":'$freq'}'
+		local json_data='{"type":"request","router":"'$router'","time":"'$time'","signal":'$signal',"bssid":"'$bssid'","dst":"'$da'","src":"'$sa'","freq":'$freq'}'
 		# json_array=$json_array$json_data","
 		send_data $json_data
 	fi
 }
 
 function send_data {
-	#json_array=${json_array::-1}']'
 
-	# echo $json_data
+	 echo $json_data
 
-	curl -i \
-	-H "Content-Type:application/json" \
-	-X POST --data $json_data \
-	$server_full_url'/api/adddump/'
-	json_array=0-9
+	# curl -i \
+	# -H "Content-Type:application/json" \
+	# -X POST --data $json_data \
+	# $server_full_url'/api/adddump/'
+	# json_array=0-9
 }
 
 
 function start_dumping () {
-	#local starttime="$(date +%s)"
 	$dump_command | while read dump_output; do
 
 		if [[ $dump_output == *"Request"* ]]
 	   		then
 	   		parse_request $dump_output
 	   	fi
-
-	   # if [[ $dump_output == *"Response"* ]]
-	   # 	then
-	   # 	parse_response $dump_output
-	   # elif [[ $dump_output == *"Request"* ]]; then
-	   # 	parse_request $dump_output
-	   # fi
-
-	   #local now="$(date +%s)"
-	   #local elapsed=$((now  - starttime))
-	   #echo $elapsed
-	   #if [ $elapsed -gt $working_seconds ];
-	   	# then
-	   	# send_data
-	   	# return
-	   # fi
 	done	
-	# echo "Waiting "$idle_seconds" seconds"
-	# sleep $idle_seconds
- #   	start_dumping	
 }
 
 start_dumping
