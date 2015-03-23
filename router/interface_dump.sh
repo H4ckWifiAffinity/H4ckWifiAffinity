5003d55282d09c783ae44c747735245bcbab0b4a
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-03-23 17:29:25 +0100

    reduced the data sent

--- router/interface_dump.sh
+++ router/interface_dump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 interface=$1
-dump_command='tcpdump -e -s 256 type mgt subtype probe-resp or subtype probe-req -I -i '$interface
 router=$2
+dump_command='tcpdump -e -s 256 type mgt subtype probe-req -i '$interface
 # TODO: Validate input parameters
 working_seconds=10
 idle_seconds=20
@@ -11,7 +11,6 @@ server_url='localhost'
 server_port='3000'
 server_full_url=$server_protocol$server_url':'$server_port
 
-json_array="["
 
 function parse_request {
 	local signal=`echo $@ | grep -o '[^ ,]\+ signal' | grep -o '\-[0-9]\+'`
@@ -20,35 +19,30 @@ function parse_request {
 		local time="$(date +%s)"
 		# local router= `/sbin/ifconfig | grep \'$interface\' | tr -s ' ' | cut -d ' ' -f5`
 		# local router=$(ifconfig wlan0 | grep -o -E "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}")
-		local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+' | cut -c 7-`
-		local da=`echo $@ | grep -o 'DA:[^ ,]\+' | cut -c 4-`
+		# local bssid=`echo $@ | grep -o 'BSSID:[^ ,]\+' | cut -c 7-`
+		# local da=`echo $@ | grep -o 'DA:[^ ,]\+' | cut -c 4-`
 		local sa=`echo $@ | grep -o 'SA:[^ ,]\+' | cut -c 4-`
 		local freq=`echo $@ | grep -o '[^ ,]\+ MHz' | grep -o '[0-9]\+'`
-		local json_data='{"type":"request","router":"'$router'","time":"'$time'","signal":'$signal',"bssid":"'$bssid'","dst":"'$da'","src":"'$sa'","freq":'$freq'}'
-		# json_array=$json_array$json_data","
+		local json_data='{"type":"request","router":"'$router'","time":"'$time'","signal":'$signal',"src":"'$sa'","freq":'$freq'}'
 		send_data $json_data
 	fi
 }
 
 function send_data {
 
-	 echo $json_data
+	# echo $json_data
 
-	# curl -i \
-	# -H "Content-Type:application/json" \
-	# -X POST --data $json_data \
-	# $server_full_url'/api/adddump/'
-	# json_array=0-9
+	curl -i \
+	-H "Content-Type:application/json" \
+	-X POST --data $json_data \
+	$server_full_url'/api/adddump/'
+	json_array=0-9
 }
 
 
 function start_dumping () {
 	$dump_command | while read dump_output; do
-
-		if [[ $dump_output == *"Request"* ]]
-	   		then
-	   		parse_request $dump_output
-	   	fi
+		parse_request $dump_output
 	done	
 }
 
